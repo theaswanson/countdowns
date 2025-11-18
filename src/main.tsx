2482195ffbd5b8153eae90e28d50133357d68bd8
Adam Swanson <adam.swanson.98@gmail.com>
2025-11-17 19:39:52 -0500

    Fix router basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { App } from "./App.tsx";
 import { Kirby } from "./components/Kirby.tsx";
 import { MetroidPrime4 } from "./components/MetroidPrime4.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='countdowns'>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/kirby-air-riders" element={<Kirby />} />
-        <Route path="/metroid-prime-4" element={<MetroidPrime4 />} />
+        <Route path='/' element={<App />} />
+        <Route path='/kirby-air-riders' element={<Kirby />} />
+        <Route path='/metroid-prime-4' element={<MetroidPrime4 />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
